03dee42c7394a2096f3407991fcc5553e6c99850
Kevin Deisz <kevin.deisz@gmail.com>
2020-01-25 14:50:31 -0500

    Test out whitespace insensitivity

--- test/format.test.js
+++ test/format.test.js
@@ -2,10 +2,19 @@ const fs = require("fs");
 const path = require("path");
 const prettier = require("prettier");
 
-test("fixture", () => {
-  const filepath = path.join(__dirname, "./fixture.xml");
-  const content = fs.readFileSync(filepath, "utf-8");
+const fixture = fs.readFileSync(path.join(__dirname, "./fixture.xml"), "utf-8");
+const format = (content, opts = {}) => prettier.format(content, {
+  ...opts,
+  parser: "xml",
+  plugins: ["."]
+});
 
-  const formatted = prettier.format(content, { parser: "xml", plugins: ["."] });
+test("defaults", () => {
+  const formatted = format(fixture);
+  expect(formatted).toMatchSnapshot();
+});
+
+test("xmlWhitespaceSensitivity => ignore", () => {
+  const formatted = format(fixture, { xmlWhitespaceSensitivity: "ignore" });
   expect(formatted).toMatchSnapshot();
 });
