d98ddd5895a39b01cb1fbe4b8e968ccb5404f2b5
Kevin Deisz <kevin.deisz@gmail.com>
2020-01-28 08:53:51 -0500

    Reformat with prettier

--- test/format.test.js
+++ test/format.test.js
@@ -3,11 +3,12 @@ const path = require("path");
 const prettier = require("prettier");
 
 const fixture = fs.readFileSync(path.join(__dirname, "./fixture.xml"), "utf-8");
-const format = (content, opts = {}) => prettier.format(content, {
-  ...opts,
-  parser: "xml",
-  plugins: ["."]
-});
+const format = (content, opts = {}) =>
+  prettier.format(content, {
+    ...opts,
+    parser: "xml",
+    plugins: ["."]
+  });
 
 test("defaults", () => {
   const formatted = format(fixture);
